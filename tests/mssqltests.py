10dd3e4fda4381b76ea080c840b3d93d99f115d4
damoxc <damoxc@cef11e0c-2622-11df-baa9-c5a8049e1931>
2010-03-03 11:31:54 +0000

    add the possibility to use a config file for specifying servers for tests

--- tests/mssqltests.py
+++ tests/mssqltests.py
@@ -1,10 +1,26 @@
-import unittest
+import sys
 import _mssql
+import unittest
+import ConfigParser
 
-server = 'localhost'
-username = 'sa'
-password = ''
-database = 'tempdb'
+_parser = ConfigParser.ConfigParser({
+    'server': 'localhost',
+    'username': 'sa',
+    'password': '',
+    'database': 'tempdb'
+})
+_parser.read('tests.cfg')
+
+if sys.argv[1:] and _parser.has_section(sys.argv[1]):
+    section = sys.argv[1]
+else:
+    section = 'DEFAULT'
+
+server = _parser.get(section, 'server')
+username = _parser.get(section, 'username')
+password = _parser.get(section, 'password')
+database = _parser.get(section, 'database')
+
 
 class MSSQLTestCase(unittest.TestCase):
     def setUp(self):
