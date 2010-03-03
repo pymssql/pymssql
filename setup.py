8e6ff1957b026108bb908db34fdc689c9d660540
damoxc <damoxc@cef11e0c-2622-11df-baa9-c5a8049e1931>
2010-03-03 11:32:29 +0000

    add setuptools bootstrap script and add checking for setuptools in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,37 @@
 #!/usr/bin/env python
+#
+# setup.py
+#
+# Copyright (C) 2009 Damien Churchill <damoxc@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.    If not, write to:
+#   The Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor
+#   Boston, MA    02110-1301, USA.
+#
+
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+    from setuptools import setup, find_packages, Extension
 
 import os
 import sys
-from setuptools import setup, Extension
+from distutils import cmd, sysconfig
+from distutils.command.clean import clean as _clean
 from Cython.Distutils import build_ext
 
 _extra_compile_args = [
