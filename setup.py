f637f16635dc2996c12bdd7a517badea35220968
damoxc <damoxc@cef11e0c-2622-11df-baa9-c5a8049e1931>
2010-03-03 11:30:38 +0000

    bump version to 1.9.903

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ if sys.platform == 'darwin':
 
 setup(
     name  = 'pymssql',
-    version = '1.9.901',
+    version = '1.9.903',
     description = 'A simple database interface to MS-SQL for Python.',
     long_description = 'A simple database interface to MS-SQL for Python.',
     author = 'Damien Churchill',
