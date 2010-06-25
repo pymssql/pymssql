46f684a75c8060915762641d755c24f81e23aa06
damoxc <damoxc@cef11e0c-2622-11df-baa9-c5a8049e1931>
2010-06-25 11:11:26 +0000

    add some threaded tests that cause errors

--- tests/threaded.py
+++ tests/threaded.py
@@ -21,12 +21,50 @@ class TestingThread(threading.Thread):
         finally:
             self.running = False
 
+class TestingErrorThread(threading.Thread):
+
+    def run(self):
+        self.running = True
+        self.exc = None
+        try:
+            mssql = _mssql.connect(server, username, password)
+            mssql.select_db(database)
+            for i in xrange(0, 1000):
+                try:
+                    mssql.execute_query('SELECT unknown_column')
+                except:
+                    pass
+            mssql.close()
+        except Exception, e:
+            self.exc = e
+        finally:
+            self.running = False
+
+class SprocTestingErrorThread(threading.Thread):
+
+    def run(self):
+        self.running = True
+        self.exc = None
+        try:
+            mssql = _mssql.connect(server, username, password)
+            mssql.select_db(database)
+            for i in xrange(0, 1000):
+                try:
+                    proc = mssql.init_procedure('pymssqlErrorThreadTest')
+                    proc.execute()
+                except:
+                    pass
+            mssql.close()
+        except Exception, e:
+            self.exc = e
+        finally:
+            self.running = False
 
 class ThreadedTests(unittest.TestCase):
     
     def testThreadedUse(self):
         threads = []
-        for i in xrange(0, 5):
+        for i in xrange(0, 50):
             thread = TestingThread()
             thread.start()
             threads.append(thread)
@@ -40,6 +78,55 @@ class ThreadedTests(unittest.TestCase):
                 if thread.running:
                     running = True
                     break
+
+    def testErrorThreadedUse(self):
+        threads = []
+        for i in xrange(0, 2):
+            thread = TestingErrorThread()
+            thread.start()
+            threads.append(thread)
+
+        running = True
+        while running:
+            running = False
+            for thread in threads:
+                if thread.exc:
+                    raise thread.exc
+                if thread.running:
+                    running = True
+                    break
+
+    def testErrorSprocThreadedUse(self):
+
+        mssql = _mssql.connect(server, username, password)
+        mssql.select_db(database)
+        mssql.execute_non_query("""
+        CREATE PROCEDURE [dbo].[pymssqlErrorThreadTest]
+        AS
+        BEGIN
+            SELECT unknown_column FROM unknown_table;
+        END
+        """)
+
+        threads = []
+        for i in xrange(0, 5):
+            thread = SprocTestingErrorThread()
+            thread.start()
+            threads.append(thread)
+
+        try:
+            running = True
+            while running:
+                running = False
+                for thread in threads:
+                    if thread.exc:
+                        raise thread.exc
+                    if thread.running:
+                        running = True
+                        break
+        finally:
+            mssql.execute_non_query("DROP PROCEDURE [dbo].[pymssqlThreadTest]")
+            mssql.close()
 
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(ThreadedTests))
