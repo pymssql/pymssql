1f9ae59267df382eb111a3fab7265c5a15562ac4
Marc Abramowitz <marc@marc-abramowitz.com>
2013-01-09 16:30:53 -0800

    bug 18 ("FetchAll fails to return rows when running against a connection instantiated with as_dict=True."): Add a test which illustrates that the issue is resolved.

---
+++ tests/test_connection_as_dict.py
@@ -0,0 +1,29 @@
+import unittest
+
+import pymssql
+
+from .helpers import config
+
+def pymssqlconn(**kwargs):
+    return pymssql.connect(
+            server=config.server,
+            user=config.user,
+            password=config.password,
+            database=config.database,
+            port=config.port,
+            charset='UTF-8',
+            **kwargs
+        )
+
+
+class TestConnectionAsDict(unittest.TestCase):
+
+    def setUp(self):
+        self.conn = pymssqlconn(as_dict=True)
+
+    def test_fetchall_with_connection_as_dict(self):
+        # This test is for http://code.google.com/p/pymssql/issues/detail?id=18
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT 'foo' AS first_name, 'bar' AS last_name")
+        data = cursor.fetchall()
+        self.assertEquals(data, [{'first_name': u'foo', 'last_name': u'bar'}])
