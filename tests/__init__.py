3d34a64406b853aad8277fd5f8dc1fb43e99fac5
Marc Abramowitz <marc@marc-abramowitz.com>
2013-09-22 22:04:17 -0700

    tests/__init__.py: Get an app lock for the entire duration of tests to prevent Travis CI jobs from running concurrently and clobbering each other

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,29 @@
+from .helpers import mssqlconn
 
+MSSQLCONN = None
+
+def setup_package():
+    global MSSQLCONN
+
+    MSSQLCONN = mssqlconn()
+    get_app_lock(MSSQLCONN)
+
+def teardown_package():
+    release_app_lock(MSSQLCONN)
+
+def get_app_lock(mssqlconn):
+    print("*** Grabbing app lock for pymssql tests")
+    mssqlconn.execute_non_query("""
+    sp_getapplock
+        @Resource = 'pymssql_tests',
+        @LockMode = 'Exclusive',
+        @LockOwner = 'Session';
+    """)
+
+def release_app_lock(mssqlconn):
+    print("*** Releasing app lock for pymssql tests")
+    mssqlconn.execute_non_query("""
+    sp_releaseapplock
+        @Resource = 'pymssql_tests',
+        @LockOwner = 'Session';
+    """)
