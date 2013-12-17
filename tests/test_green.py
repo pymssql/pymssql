24a075ef84e309f0eb26263f69e1163aec73f084
Marc Abramowitz <marc@marc-abramowitz.com>
2013-12-17 08:21:12 -0800

    tests/test_green.py: Run tests for pymssql.execute and _mssql.execute_query

--- tests/test_green.py
+++ tests/test_green.py
@@ -9,12 +9,12 @@ except ImportError:
     raise SkipTest('gevent is not available')
 
 import pymssql
-from .helpers import pymssqlconn
+from .helpers import mssqlconn, pymssqlconn
 
 
 class GreenletTests(unittest.TestCase):
 
-    def greenlet_run(self, num):
+    def greenlet_run_pymssql_execute(self, num):
         with pymssqlconn() as conn:
             cur = conn.cursor()
             cur.execute("""
@@ -23,14 +23,24 @@ class GreenletTests(unittest.TestCase):
             """)
             row = cur.fetchone()
 
-    def _run_all_greenlets(self):
+    def greenlet_run_mssql_execute(self, num):
+        conn = mssqlconn()
+        conn.execute_query("""
+        WAITFOR DELAY '00:00:05'  -- sleep for 5 seconds
+        SELECT CURRENT_TIMESTAMP
+        """)
+        for row in conn:
+            pass
+        conn.close()
+
+    def _run_all_greenlets(self, greenlet_task):
         greenlets = []
 
         dt1 = datetime.datetime.now()
 
         for i in range(5):
             gevent.sleep(1)
-            greenlets.append(gevent.spawn(self.greenlet_run, i))
+            greenlets.append(gevent.spawn(greenlet_task, i))
 
         gevent.joinall(greenlets)
 
@@ -38,13 +48,27 @@ class GreenletTests(unittest.TestCase):
 
         return dt2 - dt1
 
-    def test_gevent_socket_wait_read_concurrency(self):
+    def test_gevent_socket_pymssql_execute_wait_read_concurrency(self):
         def wait_callback(read_fileno):
             gevent.socket.wait_read(read_fileno)
 
         pymssql.set_wait_callback(wait_callback)
 
-        elapsed_time = self._run_all_greenlets()
+        elapsed_time = self._run_all_greenlets(
+            self.greenlet_run_pymssql_execute)
+
+        self.assertTrue(
+            elapsed_time < datetime.timedelta(seconds=20),
+            'elapsed_time < 20 seconds')
+
+    def test_gevent_socket_mssql_execute_wait_read_concurrency(self):
+        def wait_callback(read_fileno):
+            gevent.socket.wait_read(read_fileno)
+
+        pymssql.set_wait_callback(wait_callback)
+
+        elapsed_time = self._run_all_greenlets(
+            self.greenlet_run_mssql_execute)
 
         self.assertTrue(
             elapsed_time < datetime.timedelta(seconds=20),
