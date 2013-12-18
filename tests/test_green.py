e63d95b902be7197b02437b40075cde505be2451
Marc Abramowitz <marc@marc-abramowitz.com>
2013-12-17 19:49:58 -0800

    Add test for green execution of stored procs with callproc

--- tests/test_green.py
+++ tests/test_green.py
@@ -23,6 +23,29 @@ class GreenletTests(unittest.TestCase):
             """)
             row = cur.fetchone()
 
+    def greenlet_run_pymssql_callproc(self, num):
+        with pymssqlconn() as conn:
+            cur = conn.cursor()
+            proc_name = 'my_proc'
+            # print("~~ Checking for stored proc (num=%r)..." % num)
+            # cur.execute("IF OBJECT_ID('%s', 'P') IS NOT NULL DROP PROCEDURE %s" % (proc_name, proc_name))
+            # print("~~ Creating stored proc (num=%r)..." % num)
+            # cur.execute("""
+            # CREATE PROCEDURE %s AS
+            # BEGIN
+            #     SET NOCOUNT ON
+            #     WAITFOR DELAY '00:00:05'  -- sleep for 5 seconds
+            #     SELECT CURRENT_TIMESTAMP
+            # END
+            # """ % (proc_name,))
+            # print("~~ Calling stored proc (num=%r)..." % num)
+            cur.callproc(proc_name, ())
+            # print("~~ callproc returned (num=%r)..." % num)
+            cur.nextset()
+            row = cur.fetchone()
+            print("greenlet_run_pymssql_callproc: num = %r; row = %r" % (num, row))
+            cur.close()
+
     def greenlet_run_mssql_execute(self, num):
         conn = mssqlconn()
         conn.execute_query("""
@@ -56,6 +79,33 @@ class GreenletTests(unittest.TestCase):
 
         elapsed_time = self._run_all_greenlets(
             self.greenlet_run_pymssql_execute)
+
+        self.assertTrue(
+            elapsed_time < datetime.timedelta(seconds=20),
+            'elapsed_time < 20 seconds')
+
+    def test_gevent_socket_pymssql_callproc_wait_read_concurrency(self):
+        def wait_callback(read_fileno):
+            gevent.socket.wait_read(read_fileno)
+
+        pymssql.set_wait_callback(wait_callback)
+
+        with pymssqlconn() as conn:
+            cur = conn.cursor()
+            proc_name = 'my_proc'
+            cur.execute("IF OBJECT_ID('%s', 'P') IS NOT NULL DROP PROCEDURE %s" % (proc_name, proc_name))
+            cur.execute("""
+            CREATE PROCEDURE %s AS
+            BEGIN
+                SET NOCOUNT ON
+                WAITFOR DELAY '00:00:05'  -- sleep for 5 seconds
+                SELECT CURRENT_TIMESTAMP
+            END
+            """ % (proc_name,))
+            conn.commit()
+
+        elapsed_time = self._run_all_greenlets(
+            self.greenlet_run_pymssql_callproc)
 
         self.assertTrue(
             elapsed_time < datetime.timedelta(seconds=20),
