02a3a70f93836b767d14fa97d8ac01f666eed459
Marc Abramowitz <marc@marc-abramowitz.com>
2014-01-07 13:28:13 -0800

    Travis won't work for PRs from forks: make clear
    Make Travis CI fail if "$TRAVIS_SECURE_ENV_VARS" = "false"

--- bin/travis_env.sh
+++ bin/travis_env.sh
@@ -1,3 +1,10 @@
+if [ "$TRAVIS_SECURE_ENV_VARS" = "false" ]; then
+    echo "---------------------------------------------------------------------------------------------------------------------------------------" 1>&2
+    echo "\$TRAVIS_SECURE_ENV_VARS is set to \"$TRAVIS_SECURE_ENV_VARS\"; probably running in a pull request from a fork; this is not supported by Travis CI. Failing." 1>&2
+    echo "---------------------------------------------------------------------------------------------------------------------------------------" 1>&2
+    exit 1
+fi
+
 case $TRAVIS_PYTHON_VERSION in
     2.5)
         PYMSSQL_TEST_DATABASE=pymssql_dev_py25
