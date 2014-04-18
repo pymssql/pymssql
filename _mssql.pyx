6b2a1eaa9d5fecdfb44b8d690bc2b72e1a787a6d
Ramiro Morales <ramiro@rmorales.net>
2014-04-17 21:45:09 -0300

    Fix tds_version _mssql connection property value for TDS 7.2.
    Refs 32584060 and a8f58fa1.

--- _mssql.pyx
+++ _mssql.pyx
@@ -503,9 +503,9 @@ cdef class MSSQLConnection:
         def __get__(self):
             cdef int version = dbtds(self.dbproc)
             if version == 10:
-                return 9.0
+                return 7.2
             elif version == 9:
-                return 8.0
+                return 8.0  # Actually 7.1, return 8.0 to keep backward compatibility
             elif version == 8:
                 return 7.0
             elif version == 6:
--- docs/ref/_mssql.rst
+++ docs/ref/_mssql.rst
@@ -133,7 +133,7 @@ Functions
 .. attribute:: MSSQLConnection.tds_version
 
    The TDS version used by this connection. Can be one of ``4.2``, ``5.0``
-   ``7.0``, ``8.0`` and ``9.0``.
+   ``7.0``, ``8.0`` and ``7.2``.
 
 ``MSSQLConnection`` object methods
 ----------------------------------
