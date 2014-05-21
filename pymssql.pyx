b0a31ffa52df8b76942b2aa747d166e46aab27fe
Steve Foley <sfoley1988@gmail.com>
2014-05-21 17:54:23 -0400

    In fetch*(), increment _rownumber after getrow()
    Fixes #141

--- pymssql.pyx
+++ pymssql.pyx
@@ -490,7 +490,9 @@ cdef class Cursor:
             raise OperationalError('Statement not executed or executed statement has no resultset')
 
         try:
-            return self.getrow()
+            row = self.getrow()
+            self._rownumber += 1
+            return row
 
         except StopIteration:
             return None
@@ -512,6 +514,7 @@ cdef class Cursor:
             for i in xrange(size):
                 try:
                     rows.append(self.getrow())
+                    self._rownumber += 1
                 except StopIteration:
                     break
             return rows
