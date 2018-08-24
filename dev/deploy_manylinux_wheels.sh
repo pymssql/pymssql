c78ba65c3b677949ab3c653910a8485384b8a2f8
Alex Hagerman <alex@unexpectedeof.net>
2018-08-23 22:11:22 -0400

    Add standard .pypirc. Change ptest path call. Add deployment step pointing to test.pypi.org on tag.

--- dev/deploy_manylinux_wheels.sh
+++ dev/deploy_manylinux_wheels.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Pull in Windows artifacts from appveyor https://ci.appveyor.com/project/level12/pymssql if publishing
-
-# Upload the wheels to test.pypi.org
-# twine upload --repository-url https://test.pypi.org/legacy/ dist/*
-
 # Test installing from test.pypi.org
 # pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple --pre pymssql
+
+if [[ -z "$CIRCLE_TAG" ]]; then
+  echo $CIRCLE_TAG
+  pip install twine --upgrade
+  twine upload -u $TEST_PYPI_USERNAME -p $TEST_PYPI_PASSWORD --repository testpypi dist/*
+else
+  echo "Not a tagged release $CIRCLE_TAG"
+fi
+
