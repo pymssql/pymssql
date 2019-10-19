b12a8e04d35e52ea3bb9cf410a2243765f8d0f6b
Patrick Hofmann <patrick@ph89.de>
2019-10-19 22:43:55 +0200

    Removes platform.linux_distribution output, since its not longer existing in python 3.8.

--- setup.py
+++ setup.py
@@ -156,8 +156,6 @@ SYSTEM = platform.system()
 
 print("setup.py: platform.system() => %r" % SYSTEM)
 print("setup.py: platform.architecture() => %r" % (platform.architecture(),))
-if SYSTEM == 'Linux':
-    print("setup.py: platform.linux_distribution() => %r" % (platform.linux_distribution(),))
 if SYSTEM != 'Windows':
     print("setup.py: platform.libc_ver() => %r" % (platform.libc_ver(),))
 
