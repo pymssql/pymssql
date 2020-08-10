ab57c57689ef4b5fc19057e7db39ae3ede0c0f73
Mikhail Terekhov <termim@gmail.com>
2020-08-10 17:43:20 -0400

    Typo in get_sql_server_version

--- tests/helpers.py
+++ tests/helpers.py
@@ -483,7 +483,7 @@ def get_sql_server_version(mssql_connection):
     ver_code = int(result.split('.')[0])
     if ver_code >= 12:
         major_version = 2014
-    if ver_code == 11:
+    elif ver_code == 11:
         major_version = 2012
     elif ver_code == 10:
         major_version = 2008
