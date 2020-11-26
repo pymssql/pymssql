68ccab111829450910ca7d927c661f0870dd273a
Mikhail Terekhov <termim@gmail.com>
2020-11-26 15:06:22 -0500

    Print all version information in print_versions.py.

--- src/print_versions.py
+++ src/print_versions.py
@@ -1,11 +1,30 @@
-import _mssql
+# -*- coding: utf-8 -*-
+
 import pymssql
+import _mssql
 
-print('pymssql.__full_version__ = %r' % pymssql.__full_version__)
-print('_mssql.__full_version__ = %r' % _mssql.__full_version__)
-print('pymssql.get_dbversion() => %r' % pymssql.get_dbversion())
+def main():
 
-try:
-    print('pymssql.get_freetds_version() => %r' % pymssql.get_freetds_version())
-except AttributeError:
-    pass
+    print("pymssql:")
+    print("VERSION            :", pymssql.VERSION)
+    print("version            :", pymssql.__version__)
+    print("full_version       :", pymssql.__full_version__)
+    print("dbversion          :", pymssql.get_dbversion())
+    try:
+        print("get_freetds_version :", pymssql.get_freetds_version())
+    except AttributeError:
+        pass
+
+    print("_mssql:")
+    print("VERSION            :", _mssql.VERSION)
+    print("version            :", _mssql.__version__)
+    print("full_version       :", _mssql.__full_version__)
+    print("dbversion          :", _mssql.get_dbversion())
+    print("login_timeout      :", _mssql.login_timeout)
+    print("min_error_severity :", _mssql.min_error_severity)
+
+
+
+if __name__ == '__main__':
+
+    main()
