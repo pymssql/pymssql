88966638a5b03c1bec1bfb2e96613a250f442ec2
PrimozGodec <p.godec9@gmail.com>
2023-08-16 09:14:11 +0200

    Fix OpenSLL library path

--- setup.py
+++ setup.py
@@ -167,7 +167,7 @@ class build_ext(_build_ext):
                     if BITNESS == 32:
                         e.library_dirs.append("c:/Program Files (x86)/OpenSSL-Win32/lib")
                     else:
-                        e.library_dirs.append("c:/Program Files/OpenSSL-Win64/lib")
+                        e.library_dirs.append("c:/Program Files/OpenSSL/lib")
 
         else:
             if LINK_KRB5:
