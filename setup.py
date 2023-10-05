a4406b672896078aeccfc36fd05f614a78c634bb
Raphael Jacob <r.jacob2002@gmail.com>
2023-10-05 17:06:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -319,6 +319,7 @@ setup(
       "Programming Language :: Python :: 3.9",
       "Programming Language :: Python :: 3.10",
       "Programming Language :: Python :: 3.11",
+      "Programming Language :: Python :: 3.12",
       "Programming Language :: Python :: Implementation :: CPython",
       "Topic :: Database",
       "Topic :: Database :: Database Engines/Servers",
