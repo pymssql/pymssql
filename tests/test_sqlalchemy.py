2d3604db2140edd161c0d0481106742702c3aadb
Timotheus Bachinger <timotheus.bachinger@checkmk.com>
2025-05-12 10:02:26 +0200

    Fix sqlalchemy.orm.exc.MappedAnnotationError
    see https://docs.sqlalchemy.org/en/20/errors.html#type-annotation-can-t-be-interpreted-for-annotated-declarative-table-form

--- tests/test_sqlalchemy.py
+++ tests/test_sqlalchemy.py
@@ -34,6 +34,7 @@ Session = sessionmaker(bind=engine)
 
 class SAObj(Base):
     __tablename__ = 'sa_test_objs'
+    __allow_unmapped__ = True
     id = sa.Column(sa.Integer, primary_key=True)
     name = sa.Column(sa.String(50))
     data = sa.Column(sa.PickleType)
